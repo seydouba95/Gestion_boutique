ffb83b98daa25899b5e0bbc2cba184e62f879ebe
seydou <baseydou95@gmail.com>
2020-04-17 20:11:29 +0000

    test de la fonctionnalite du bouton vendre

--- app/Http/Controllers/VenteController.php
+++ app/Http/Controllers/VenteController.php
@@ -9,20 +9,26 @@ use App\Vente;
 class VenteController extends Controller
 {
     
-   public function vente($id){
+   public function vente($id)
+   {
   
     $produit = Produit::find($id);
     if( $_POST['quantiteVendre'] <= $produit->stock) {
         
       $res= "Achat effectuee";
-      $produit->stock = ($produit->stock) - ($_POST['quantiteVendre']) ;
+      $stock = $produit->stock = ($produit->stock) - ($_POST['quantiteVendre']) ;
         echo $res . " ";
         echo ("le reste est : " . " " .$produit->stock);
-    }
-      
+        Produit::where('id',$id)->update(['stock'=> $stock]);
+    } 
+    
     else
        echo "le nombre de produit en stock est insuffisant pour cette commande";
-    
-   
+       
+    if($produit->stock == 0)
+    { $status = "disabled" ;
+        
+    } 
+     return redirect('/');
 }
 }
--- resources/views/venteProduit.blade.php
+++ resources/views/venteProduit.blade.php
@@ -1,21 +1,30 @@
-<a  type="button" class="btn btn-sm btn-success" data-toggle="modal" data-target="#exampleModal{{$produit->id}}">Vendre</a>
 
+<a  type="button" disabled="'.$status.'" class="btn btn-sm btn-success" data-toggle="modal" data-target="#exampleModal{{$produit->id}}">Vendre</a>
+<?PHP 
+$produit = App\Produit::find($produit->id);
+if ($produit->stock == 0) { $status = "enabled" ; } else { $status = "disabled" ;}
+?>
 
 <div class="modal fade" id="exampleModal{{$produit->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
 
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModal{{$produit->id}}">Information sur  le produit </h5>
+                <h5 class="modal-title" id="exampleModal{{$produit->id}}">Vente {{$produit->nom}} </h5>
 
             </div>
             <div class="modal-body">
                 
 
             <form action="{{url('/vente/'.$produit->id)}}" method="post">    
+                       
                                @csrf
+                               <div class="form-group">
+                        <label class="col-form-label">Quantité en stock :</label>
+                        <input disabled value={{$produit->stock}}  type="text" name="stock"  id="stock"  class="form-control">
+                    </div>
                     <div class="form-group">
-                        <label class="col-form-label">Quantite:</label>
+                        <label class="col-form-label">Quantité à vendre:</label>
                         <input  type="text" name="quantiteVendre"  id="quantiteVendre"  class="form-control">
                     </div>
 
@@ -23,7 +32,8 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                <button type="submit" class="btn btn-primary">Acheter</button>
+              
+                <button  type="submit" class="btn btn-primary">Vendre</button>
             </div>
             </form>
         </div>
