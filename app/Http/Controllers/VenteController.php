3d1156db201b206a05bb9fe34474c3939a4692d7
seydou <baseydou95@gmail.com>
2020-04-18 01:21:43 +0000

    validation formulaire vente

--- app/Http/Controllers/VenteController.php
+++ app/Http/Controllers/VenteController.php
@@ -20,13 +20,17 @@ class VenteController extends Controller
       $stock = $produit->stock = ($produit->stock) - ($_POST['quantiteVendre']) ;
 
         Produit::where('id',$id)->update(['stock'=> $stock]);
+        if($_POST['quantiteVendre'] >1 )
         return redirect('/')->with('success',  $_POST['quantiteVendre'] . " " .  'Produits ' .$produit->nom . " " .'vendus' . " " . 'avec succès');      
+         else
+         return redirect('/')->with('success',  $_POST['quantiteVendre'] . " " .  'Produit ' .$produit->nom . " " .'vendu' . " " . 'avec succès');      
 
     } 
     
     else
     return redirect('/')->with('success',' ce  stock du Produit ' .$produit->nom . " " .'n\'est pas disponible ' . " ");      
-
+   
+    
        
   
    }
